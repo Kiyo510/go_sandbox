f0bdf297c639ce93e53a0c689d2c424d32ab64b1
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-05-29 01:51:36 +0900

    t:sparkles: timeパッケージについて復習

--- src/main.go
+++ src/main.go
@@ -3,34 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
+	"time"
 )
 
-func handleData(conn net.Conn) {
-	defer conn.Close()
-
-	var b [512]byte
-	for {
-		n, err := conn.Read(b[:])
-		if err != nil {
-			break
-		}
-		fmt.Println(string(b[:n]))
-	}
-}
-
 func main() {
-	lis, err := net.Listen("tcp", ":8888")
+	//now := time.Now()
+	//fmt.Println(now.Format("2006/01/02 15:04:05"))
+	//fmt.Println(now.Format("2006/1/02 03:04:05"))
+	//fmt.Println(now.Format(time.RFC3339))
+
+	//var s = "2022/12/25 07:42:38"
+	//d, err := time.Parse("2006/01/02 15:04:05", s)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(d)
+
+	d, err := time.ParseDuration("3s")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer lis.Close()
 
-	for {
-		conn, err := lis.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		go handleData(conn)
+	fmt.Println(d)
+
+	d, err = time.ParseDuration("4m")
+	if err != nil {
+		log.Fatal(d)
 	}
+	fmt.Println(d / 2)
 }
