f7043a5177f6547498871cc79723b14087391d02
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-05-29 02:50:49 +0900

    echoとgo:embedの学習

--- src/main.go
+++ src/main.go
@@ -1,49 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io/fs"
-	"log"
-	"os"
-	"path/filepath"
+	_ "embed"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
+//go:embed static/logo.png
+var contents []byte
+
 func main() {
-	//println(filepath.Base("/src/file.txt"))
-	//println(filepath.Dir("/src/file.txt"))
-	//println(filepath.Clean("/src/../file.txt"))
-	//println(filepath.Ext("/src/file.txt"))
-	//println(filepath.IsAbs("/src/file.txt"))
-	//println(filepath.IsAbs("./src/file.txt"))
-	//println(filepath.Join("/src", "/file.txt"))
-	//absolute, err := filepath.Abs("../file.txt")
-	//if err == nil {
-	//	println(absolute)
-	//}
-	//
-	//absolute, err = filepath.Rel("/src", "/src/file.txt")
-	//if err == nil {
-	//	println(absolute)
-	//}
-
-	var files []string
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = filepath.WalkDir(cwd, func(path string, info fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		files = append(files, path)
-		return nil
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.Blob(http.StatusOK, "image/png", contents)
 	})
-
-	fmt.Println(files)
+	e.Logger.Fatal(e.Start(":8989"))
 }
---
+++ src/mylib.h
@@ -0,0 +1,16 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void mylib_update_data(const char* s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MYLIB_H */
