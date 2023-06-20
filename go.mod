0d55f5a115d17ce6595222be0b39c7edecd6f1f4
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-06-21 04:40:30 +0900

    mapsパッケージを試してみた

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/Kiyo510/go-sandbox
 
 go 1.20
 
-require github.com/gocraft/web v0.0.0-20190207150652-9707327fb69b
-
-require github.com/stretchr/testify v1.8.4 // indirect
+require golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
--- src/main.go
+++ src/main.go
@@ -2,50 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"golang.org/x/exp/maps"
 )
 
-func fanIn(ch1, ch2 <-chan string) <-chan string {
-	newCh := make(chan string)
-	go func() {
-		for {
-			select {
-			case s := <-ch1:
-				newCh <- s
-			case s := <-ch2:
-				newCh <- s
-			default:
-				//update_progressbar()
-			}
-		}
-	}()
-
-	return newCh
-}
-
 func main() {
-	quit := make(chan string)
-	ch := generator("Hi!", quit)
-	for i := rand.Intn(50); i >= 0; i-- {
-		fmt.Println(<-ch, i)
-	}
-
-	quit <- "Bye"
-	fmt.Printf("%s", <-quit)
-}
-
-func generator(msg string, quit chan string) <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		for i := 0; ; i++ {
-			select {
-			case ch <- fmt.Sprintf("%s", msg):
-			case <-quit:
-				quit <- "See you!"
-				return
-			}
-		}
-	}()
-	return ch
+	persons := map[string]int{"伊藤": 10, "佐藤": 22, "鈴木": 2999}
+	fmt.Println(maps.Keys(persons))
 }
