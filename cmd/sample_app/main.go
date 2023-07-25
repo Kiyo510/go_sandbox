f057e090de7f0834e72ae18b476813c52a445081
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-07-25 23:35:47 +0900

    値オブジェクトみたいな使い方

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -2,27 +2,33 @@ package main
 
 import "fmt"
 
-type Consumer struct {
-	ActiveFlg bool
-}
-type Consumers []Consumer
+type SKUCode string
 
-func (c Consumers) ActiveCustomer() Consumers {
-	resp := make([]Consumer, 0, len(c))
-	for _, v := range c {
-		if v.ActiveFlg {
-			resp = append(resp, v)
-		}
-	}
-	return resp
+func (c SKUCode) Invalid() bool {
+	// 桁数などのチェック
+	return len(c) != 9 // SKUコードが9桁でなければ無効とする例
+}
+
+func (c SKUCode) ItemCD() string {
+	return string(c[0:5])
+}
+
+func (c SKUCode) SizeCD() string {
+	return string(c[5:7])
+}
+
+func (c SKUCode) ColorCD() string {
+	return string(c[7:9])
 }
 
 func main() {
-	consumers := Consumers{Consumer{
-		ActiveFlg: true,
-	}, Consumer{
-		ActiveFlg: false,
-	}}
-	resp := consumers.ActiveCustomer()
-	fmt.Println(resp)
+	sku := SKUCode("123456789")
+
+	if sku.Invalid() {
+		fmt.Println("Invalid SKU Code")
+	} else {
+		fmt.Println("Item Code:", sku.ItemCD())
+		fmt.Println("Size Code:", sku.SizeCD())
+		fmt.Println("Color Code:", sku.ColorCD())
+	}
 }
