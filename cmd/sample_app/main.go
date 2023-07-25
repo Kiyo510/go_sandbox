7f9202a58be91eba7a067aececdc04852124a7ee
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-07-25 23:16:07 +0900

    スライスの型定義の実装

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -1,56 +1,28 @@
 package main
 
-import (
-	"context"
-	"log"
-	"net/http"
-	"os/signal"
-	"syscall"
-	"time"
+import "fmt"
 
-	adaptorHTTP "github.com/Kiyo510/go_sandbox/pkg/adapter/http"
-	"github.com/Kiyo510/go_sandbox/pkg/config"
-	_ "github.com/go-sql-driver/mysql"
-)
+type Consumer struct {
+	ActiveFlg bool
+}
+type Consumers []Consumer
+
+func (c Consumers) ActiveCustomer() Consumers {
+	resp := make([]Consumer, 0, len(c))
+	for _, v := range c {
+		if v.ActiveFlg {
+			resp = append(resp, v)
+		}
+	}
+	return resp
+}
 
 func main() {
-	// Create context that listens for the interrupt signal from the OS.
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	addr := config.LoadConfig().HTTPInfo.Addr
-	router := adaptorHTTP.InitRouter()
-	srv := &http.Server{
-		Addr:           addr,
-		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	// graceful shutdown
-	// https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/notify-without-context/server.go
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			log.Fatalf("Failed to listen and serve: %+v", err)
-		}
-		log.Println("server is running! addr: ", addr)
-	}()
-
-	// Listen for the interrupt signal.
-	<-ctx.Done()
-
-	// Restore default behavior on the interrupt signal and notify user of shutdown.
-	stop()
-	log.Println("shutting down gracefully, press Ctrl+C again to force")
-
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown: ", err)
-	}
-
-	log.Println("Server exiting")
+	consumers := Consumers{Consumer{
+		ActiveFlg: true,
+	}, Consumer{
+		ActiveFlg: false,
+	}}
+	resp := consumers.ActiveCustomer()
+	fmt.Println(resp)
 }
