5f961b914b452bf5a693a93d82ab14e0397c76d8
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-08-14 00:28:09 +0900

    実用Go言語: エラーハンドリング1

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -1,64 +1,18 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
-	"io"
-	"os"
-	"strings"
-
-	"golang.org/x/text/unicode/norm"
 )
 
-func Normalize(w io.Writer, r io.Reader) error {
-	br := bufio.NewReader(r)
-	for {
-		s, err := br.ReadString('\n')
-		if s != "" {
-			_, err := io.WriteString(w, norm.NFKC.String(s))
-			if err != nil {
-				return err
-			}
-		}
-		if err == io.EOF {
-			return nil
-		} else if err != nil {
-			return err
-		}
-	}
-}
+var ErrNotFound = errors.New("not found")
 
-func NormalizeFile(input, output string) error {
-	r, err := os.Open(input)
-	if err != nil {
-		return err
-	}
-	w, err := os.Create(output)
-	if err != nil {
-		return err
-	}
-	return Normalize(w, r)
-}
+func fundBook(isbn string) error {
+	return ErrNotFound
 
-func NormalizeString(i string) (string, error) {
-	r := strings.NewReader(i)
-	var w strings.Builder
-	err := Normalize(&w, r)
-	if err != nil {
-		return "", err
-	}
-	return w.String(), nil
 }
 
 func main() {
-	str, err := NormalizeString("ホゲ 　　　ｌホゲ")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(str)
-
-	err = NormalizeFile("input.txt", "output.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
+	err := fundBook("hoge")
+	fmt.Println(err)
 }
