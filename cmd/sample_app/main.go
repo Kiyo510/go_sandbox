c841b0000f5b59e72e75c3011c5afe42f65b0a0d
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-08-15 01:03:07 +0900

    実践Go言語: エラーハンドリング2 WrapとかUnwrapとか

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -3,16 +3,46 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 )
 
-var ErrNotFound = errors.New("not found")
+type HttpError struct {
+	StatusCode int
+	URL        string
+	err        error
+}
 
-func fundBook(isbn string) error {
-	return ErrNotFound
+func (he HttpError) Error() string {
+	return fmt.Sprintf("http status code = %d, URL = %s", he.StatusCode, he.URL)
+}
 
+func (he HttpError) Unwrap() error {
+	return he.err
+}
+
+func ReadContents(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, &HttpError{
+			StatusCode: resp.StatusCode,
+			URL:        url,
+			err:        errors.New("http get error"),
+		}
+	}
+	return io.ReadAll(resp.Body)
 }
 
 func main() {
-	err := fundBook("hoge")
-	fmt.Println(err)
+	body, err := ReadContents("https://google.com/hogehoge")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(body)
 }
