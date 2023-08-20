d96916f01d78dd82437e3722d9872848b1191b0f
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-08-21 01:38:40 +0900

    slogを試してみる

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -1,41 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"os"
 )
 
-type SlackServiceInterface interface {
-	Send(message string) error
-}
-
-type SlackService struct{}
-
-func (s *SlackService) Send(message string) error {
-	// 実際にSlackへ送る処理
-	fmt.Println("Sending message to Slack:", message)
-	return nil
-}
-
-type SlackDummyService struct{}
-
-func (d *SlackDummyService) Send(message string) error {
-	// ログへ出力する処理
-	fmt.Println("Logging message (dummy):", message)
-	return nil
-}
-
-func NewSlackService() SlackServiceInterface {
-	if os.Getenv("ENV") == "local" {
-		return &SlackDummyService{}
-	}
-	return &SlackService{}
-}
-
 func main() {
-	service := NewSlackService()
-	err := service.Send("Dummy message")
-	if err != nil {
-		fmt.Println(err)
-	}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("Info message") // {"time":"2023-08-21T01:37:49.760783+09:00","level":"INFO","msg":"Info message"}
 }
