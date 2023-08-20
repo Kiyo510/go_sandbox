416d594991a076748108a9438a282fa117dc0306
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-08-20 22:53:18 +0900

    実装Go言語： 4章インターフェース 環境によって動的にインスタンスを切り替える例

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -1,35 +1,41 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"os"
 )
 
-// ConvertSlices []Tを[]Uへ変換
-func ConvertSlices[T, U any](srcList []T, convertFunc func(T) (U, error)) ([]U, error) {
-	result := make([]U, len(srcList))
-	for _, v := range srcList {
-		u, err := convertFunc(v)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, u)
+type SlackServiceInterface interface {
+	Send(message string) error
+}
+
+type SlackService struct{}
+
+func (s *SlackService) Send(message string) error {
+	// 実際にSlackへ送る処理
+	fmt.Println("Sending message to Slack:", message)
+	return nil
+}
+
+type SlackDummyService struct{}
+
+func (d *SlackDummyService) Send(message string) error {
+	// ログへ出力する処理
+	fmt.Println("Logging message (dummy):", message)
+	return nil
+}
+
+func NewSlackService() SlackServiceInterface {
+	if os.Getenv("ENV") == "local" {
+		return &SlackDummyService{}
 	}
-	return result, nil
+	return &SlackService{}
 }
 
 func main() {
-	var fishList = []any{"鯖", "鰤", "鮪"}
-	strs, err := ConvertSlices(fishList, func(f any) (string, error) {
-		if fn, ok := f.(string); ok {
-			return fn, nil
-		}
-		return "", errors.New("cannot assertion")
-	})
-
+	service := NewSlackService()
+	err := service.Send("Dummy message")
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		fmt.Println(err)
 	}
-	fmt.Println(strs)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/friendsofgo/errors v0.9.2
-	github.com/gen2brain/beeep v0.0.0-20230602101333-f384c29b62dd
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.8.0
 	github.com/volatiletech/null/v8 v8.1.2
 	github.com/volatiletech/sqlboiler/v4 v4.12.0
@@ -12,16 +12,12 @@ require (
 )
 
 require (
-	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
-	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/spf13/cast v1.4.1 // indirect
-	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/volatiletech/inflect v0.0.1 // indirect
