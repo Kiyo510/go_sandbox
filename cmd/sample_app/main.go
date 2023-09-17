016a7df5fb34b126d4b45630b05fc792f02685ad
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-09-17 20:46:49 +0900

    実用Go言語 8章 マルチレイアウトCSVを処理したい

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -1,40 +1,64 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
-	"os"
+	"strings"
 
 	"github.com/gocarina/gocsv"
 )
 
-type record struct {
-	Number  int    `csv:"number"`
-	Message string `csv:"message"`
+type Summary struct {
+	RecordType string
+	Summary    string
+}
+
+type Country struct {
+	RecordType string
+	Name       string
+	ISOCode    string
+	Population int
+}
+
+type singleCSVReader struct {
+	record []string
+}
+
+func (r singleCSVReader) Read() ([]string, error) {
+	return r.record, nil
+}
+
+func (r singleCSVReader) ReadAll() ([][]string, error) {
+	return [][]string{r.record}, nil
 }
 
 func main() {
-	f, err := os.Open("test.csv")
+	s := `summary,3件
+country,アメリカ合衆国,US/USA,310232863
+county,日本,JP/JPN,127288000
+country,中国,CN/CHN,1330044000`
+
+	r := csv.NewReader(strings.NewReader(s))
+	r.FieldsPerRecord = -1
+	all, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	c := make(chan record)
-	done := make(chan bool)
-	go func() {
-		if err := gocsv.UnmarshalToChan(f, c); err != nil {
-			log.Fatal(err)
-		}
-		done <- true
-	}()
-
-	for {
-		select {
-		case v := <-c:
-			fmt.Printf("%+v\n", v)
-		case <-done:
-			return
+	for _, record := range all {
+		if record[0] == "summary" {
+			var summaries []Summary
+			if err := gocsv.UnmarshalCSVWithoutHeaders(singleCSVReader{record}, &summaries); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("summary行の読み込み: %+v\n", &summaries[0])
+		} else {
+			var countries []Country
+			if err := gocsv.UnmarshalCSVWithoutHeaders(singleCSVReader{record}, &countries); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("country行の読み込み: %+v\n", &countries[0])
 		}
 	}
 }
