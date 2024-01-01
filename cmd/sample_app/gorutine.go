79e307af44c2c7f586a21659de3caa5ceb5cee3e
Kiyo510 <miraishida00510@gmail.com>
2024-01-02 00:05:22 +0900

    study sync.Cond in Go言語による並行処理 about P55

--- cmd/sample_app/gorutine.go
+++ cmd/sample_app/gorutine.go
@@ -2,55 +2,40 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"os"
 	"sync"
-	"text/tabwriter"
 	"time"
 )
 
 func main() {
-	producer := func(wg *sync.WaitGroup, l sync.Locker) {
+	var wg sync.WaitGroup
+	var dataReady bool
+	cond := sync.NewCond(&sync.Mutex{})
+
+	// データが準備されるのを待つゴルーチン
+	waitForData := func(i int) {
 		defer wg.Done()
-		for i := 5; i > 0; i-- {
-			l.Lock()
-			l.Unlock()
-			time.Sleep(1)
+		cond.L.Lock()
+		for !dataReady {
+			cond.Wait()
 		}
+		fmt.Printf("ゴルーチン%d: データが準備されました\n", i)
+		cond.L.Unlock()
 	}
 
-	observer := func(wg *sync.WaitGroup, l sync.Locker) {
-		defer wg.Done()
-		l.Lock()
-		defer l.Unlock()
+	// 5つのゴルーチンを起動し、データが準備されるのを待つ
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go waitForData(i)
 	}
 
-	test := func(count int, mutex, rwMutex sync.Locker) time.Duration {
-		var wg sync.WaitGroup
-		wg.Add(count + 1)
-		beginTestTime := time.Now()
-		go producer(&wg, mutex)
-		for i := count; i > 0; i-- {
-			go observer(&wg, rwMutex)
-		}
+	// データを準備する
+	go func() {
+		time.Sleep(2 * time.Second) // データ準備に2秒かかるとします
+		cond.L.Lock()
+		dataReady = true
+		cond.Broadcast() // すべての待機中のゴルーチンに通知
+		cond.L.Unlock()
+	}()
 
-		wg.Wait()
-		return time.Since(beginTestTime)
-	}
-
-	tw := tabwriter.NewWriter(os.Stdout, 0, 1, 2, ' ', 0)
-	defer tw.Flush()
-
-	var m sync.RWMutex
-	fmt.Fprintf(tw, "Readers\tRWMutes\tMutex\n")
-	for i := 0; i < 20; i++ {
-		count := int(math.Pow(2, float64(i)))
-		fmt.Fprintf(
-			tw,
-			"%d\t%v\t%v\n",
-			count,
-			test(count, &m, m.RLocker()),
-			test(count, &m, &m),
-		)
-	}
+	wg.Wait()
 }
