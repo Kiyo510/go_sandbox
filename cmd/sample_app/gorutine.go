1088486592ea62458daaf0ca14eab6782bf26433
Kiyo510 <miraishida00510@gmail.com>
2024-01-02 03:15:17 +0900

    study sync.Cond in Go言語による並行処理 about P55 part2

--- cmd/sample_app/gorutine.go
+++ cmd/sample_app/gorutine.go
@@ -7,28 +7,37 @@ import (
 )
 
 func main() {
-	c := sync.NewCond(&sync.Mutex{}) // (1)
-	queue := make([]any, 0, 10)      // (2)
+	var wg sync.WaitGroup
+	var dataReady bool
+	cond := sync.NewCond(&sync.Mutex{}) //(1)
 
-	removeFromQueue := func(delay time.Duration) {
-		time.Sleep(delay)
-		c.L.Lock()        // (8)
-		queue = queue[1:] // (9)
-		fmt.Println("Removed from queue")
-		c.L.Unlock() // (10)
-		c.Signal()   // (11)
-	}
-
-	for i := 0; i < 10; i++ {
-		c.L.Lock() //(3)
-		fmt.Println("len1", len(queue))
-		for len(queue) == 2 { // (4)
-			fmt.Println("Waiting...")
-			c.Wait() // (5)
+	// データが準備されるのを待つゴルーチン
+	waitForData := func(i int) {
+		defer wg.Done()
+		cond.L.Lock()
+		for !dataReady {
+			fmt.Printf("ゴルーチン%d: データを待っています\n", i)
+			cond.Wait() //(2)
 		}
-		fmt.Println("Adding to queue")
-		queue = append(queue, struct{}{})
-		go removeFromQueue(2 * time.Second) // (6)
-		c.L.Unlock()                        // (7)
+		fmt.Printf("ゴルーチン%d: データが準備されました\n", i)
+		cond.L.Unlock()
 	}
+
+	// 5つのゴルーチンを起動し、データが準備されるのを待つ
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go waitForData(i)
+	}
+
+	// データを準備する
+	go func() {
+		fmt.Println("データの準備")
+		time.Sleep(3 * time.Second) // データ準備に2秒かかるとします
+		cond.L.Lock()
+		dataReady = true
+		cond.Broadcast() // (3)すべての待機中のゴルーチンに通知
+		cond.L.Unlock()
+	}()
+
+	wg.Wait()
 }
