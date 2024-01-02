6a007e320c1b66447bb1933c7183a149e3b81af3
Kiyo510 <miraishida00510@gmail.com>
2024-01-02 18:31:45 +0900

    study sync.Pool in Go言語による並行処理 about P60 part1

--- cmd/sample_app/gorutine.go
+++ cmd/sample_app/gorutine.go
@@ -3,41 +3,25 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
+	var count int
+
+	increment := func() {
+		count++
+	}
+
+	var once sync.Once
 	var wg sync.WaitGroup
-	var dataReady bool
-	cond := sync.NewCond(&sync.Mutex{}) //(1)
+	wg.Add(100)
 
-	// データが準備されるのを待つゴルーチン
-	waitForData := func(i int) {
-		defer wg.Done()
-		cond.L.Lock()
-		for !dataReady {
-			fmt.Printf("ゴルーチン%d: データを待っています\n", i)
-			cond.Wait() //(2)
-		}
-		fmt.Printf("ゴルーチン%d: データが準備されました\n", i)
-		cond.L.Unlock()
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			once.Do(increment)
+		}()
 	}
-
-	// 5つのゴルーチンを起動し、データが準備されるのを待つ
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go waitForData(i)
-	}
-
-	// データを準備する
-	go func() {
-		fmt.Println("データの準備")
-		time.Sleep(3 * time.Second) // データ準備に2秒かかるとします
-		cond.L.Lock()
-		dataReady = true
-		cond.Broadcast() // (3)すべての待機中のゴルーチンに通知
-		cond.L.Unlock()
-	}()
-
 	wg.Wait()
+	fmt.Printf("Count is %d\n", count)
 }
