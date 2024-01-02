287e301fb881192d0a858b2a661c01e70eca605a
Kiyo510 <miraishida00510@gmail.com>
2024-01-02 23:09:29 +0900

    study channel in Go言語による並行処理 about P68

--- cmd/sample_app/gorutine.go
+++ cmd/sample_app/gorutine.go
@@ -1,37 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
 func main() {
-	var numCalcsCreated int
-	calcPool := &sync.Pool{
-		New: func() interface{} {
-			numCalcsCreated += 1
-			mem := make([]byte, 1024)
-			return &mem
-		},
-	}
+	stringStream := make(chan string)
 
-	// put 4KB in the pool
-	calcPool.Put(calcPool.New())
-	calcPool.Put(calcPool.New())
-	calcPool.Put(calcPool.New())
-	calcPool.Put(calcPool.New())
+	go func() {
+		stringStream <- "Hello channels!"
+	}()
 
-	const numWorkers = 1024 * 1024
-	var wg sync.WaitGroup
-	wg.Add(numWorkers)
-	for i := numWorkers; i > 0; i-- {
-		go func() {
-			defer wg.Done()
-			mem := calcPool.Get().(*[]byte)
-			defer calcPool.Put(mem)
-		}()
-	}
-
-	wg.Wait()
-	fmt.Printf("%d calculators were created.", numCalcsCreated)
+	fmt.Println(<-stringStream)
 }
