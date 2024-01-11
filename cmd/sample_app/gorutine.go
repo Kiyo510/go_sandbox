f59780a575314c6ed90de0bef14c3400dff8838f
Kiyo510 <miraishida00510@gmail.com>
2024-01-11 23:16:30 +0900

    study goroutin leak 4章 in Go言語による並行処理 about 02

--- cmd/sample_app/gorutine.go
+++ cmd/sample_app/gorutine.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"sync"
-)
+import "fmt"
 
 func main() {
-	printData := func(wg *sync.WaitGroup, data []byte) {
-		defer wg.Done()
-		var buffer bytes.Buffer
-		for _, d := range data {
-			fmt.Fprintf(&buffer, "%c", d)
-		}
-		fmt.Println(buffer.String())
+	doWork := func(strings <-chan string) <-chan interface{} {
+		completed := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited.")
+			defer close(completed)
+			for s := range strings {
+				// Do something
+				fmt.Println(s)
+			}
+		}()
+		return completed
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-	data := []byte("golang")
-	printData(&wg, data[:3])
-	printData(&wg, data[3:])
-	wg.Wait()
+	doWork(nil)
+	fmt.Println("doWork completed")
 }
